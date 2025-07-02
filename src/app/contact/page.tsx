6f9af32e535d435d4b3616d708dd0133d0e3cbe7
huynhnd <huynhnd.it.22@gmail.com>
2025-07-02 19:25:54 +0700

    update phone icon

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -60,19 +60,19 @@ export default function ContactPage() {
                   </div>
                 </div>
                 
-                <div className="flex items-start">
-                  <div className="h-12 w-12 bg-primary/10 rounded-full flex items-center justify-center text-primary mr-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                    </svg>
-                  </div>
-                  {webInformation?.phone && (
-                    <div>
-                      <h3 className="text-lg font-medium mb-1">{t('contact.phone')}</h3>
-                      <p className="text-gray-600">+84 {webInformation?.phone}</p>
+                {webInformation?.phone && (
+                  <div className="flex items-start">
+                    <div className="h-12 w-12 bg-primary/10 rounded-full flex items-center justify-center text-primary mr-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                      </svg>
                     </div>
-                  )}
-                </div>
+                      <div>
+                        <h3 className="text-lg font-medium mb-1">{t('contact.phone')}</h3>
+                        <p className="text-gray-600">+84 {webInformation?.phone}</p>
+                      </div>
+                  </div>
+                )}
                 
                 <div className="flex items-start">
                   <div className="h-12 w-12 bg-primary/10 rounded-full flex items-center justify-center text-primary mr-4">
